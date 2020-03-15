3d5f427ceaac073c98584ef5e3f9471776cb5e28
DoLim <limdongyoung0@gmail.com>
2020-03-15 14:35:24 +0000

    #2.3 Finishing Up

--- main.go
+++ main.go
@@ -9,9 +9,5 @@ import (
 func main() {
 	account := accounts.NewAccount("dolim")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	if err := account.Withdraw(20); err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
