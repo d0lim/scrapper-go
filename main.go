a638e89f670ebf8ea995939080b25d3b86d3fb0c
DoLim <limdongyoung0@gmail.com>
2020-03-17 08:06:03 +0000

    Some changes. But unclear of "FAST"

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 type requestResult struct {
@@ -13,9 +14,33 @@ type requestResult struct {
 
 var errRequestFailed = errors.New("Request failed")
 
+// func main() {
+// 	results := map[string]string{}
+// 	c := make(chan requestResult)
+// 	urls := []string{
+// 		"https://www.airbnb.com/",
+// 		"https://www.google.com/",
+// 		"https://www.amazon.com/",
+// 		"https://www.reddit.com/",
+// 		"https://soundcloud.com/",
+// 		"https://www.facebook.com/",
+// 		"https://www.instagram.com/",
+// 		"https://academy.nomadcoders.co/",
+// 	}
+// 	for _, url := range urls {
+// 		go hitURL(url, c)
+// 	}
+// 	for i := 0; i < len(urls); i++ {
+// 		result := <-c
+// 		results[result.url] = result.status
+// 	}
+// 	for url, status := range results {
+// 		fmt.Println(url, status)
+// 	}
+// }
+
 func main() {
 	results := map[string]string{}
-	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -26,11 +51,8 @@ func main() {
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
 	}
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-	for i := 0; i < len(urls); i++ {
-		result := <-c
+	c := hitURLChannel(urls...)
+	for result := range c {
 		results[result.url] = result.status
 	}
 	for url, status := range results {
@@ -46,4 +68,26 @@ func hitURL(url string, c chan<- requestResult) {
 	}
 	c <- requestResult{url: url, status: status}
 
+}
+
+func hitURLChannel(urls ...string) <-chan requestResult {
+	out := make(chan requestResult)
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+	for _, url := range urls {
+		go func(out chan<- requestResult, url string) {
+			defer wg.Done()
+			resp, err := http.Get(url)
+			status := "OK"
+			if err != nil || resp.StatusCode >= 400 {
+				status = "FAILED"
+			}
+			out <- requestResult{url: url, status: status}
+		}(out, url)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
