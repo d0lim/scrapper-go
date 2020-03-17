ce1906a423517532e8333d79ebaf817bb9d5dae3
DoLim <limdongyoung0@gmail.com>
2020-03-17 14:08:00 +0000

    #5.2 File Download

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/imdigo/scrapper-go/scrapper"
 	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("index.html")
 }
 
 func handleScrap(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	return nil
+	scrapper.Scrap(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
